724f3d9d67943a91947f7eb2c6364c6a34d2d946
Lorenzo Gallucci <lgallucci@gmail.com>
2017-05-21 17:22:05 +0200

    Fixed nullability annotations

--- app/src/main/java/com/udacity/stockhawk/ui/FormattingHelper.java
+++ app/src/main/java/com/udacity/stockhawk/ui/FormattingHelper.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.RemoteViews;
 import android.widget.TextView;
@@ -64,8 +65,8 @@ public class FormattingHelper {
         });
     }
 
-    @NonNull
-    public static <T> List<T> reducePoints(@NonNull List<T> originalSeries, int maxSize) {
+    @Nullable
+    public static <T> List<T> reducePoints(@Nullable List<T> originalSeries, int maxSize) {
         if (originalSeries == null || maxSize >= originalSeries.size())
             return originalSeries;
         List<T> reducedSize = new ArrayList<>(maxSize);
